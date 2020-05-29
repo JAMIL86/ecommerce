76805ac7ea29911fa7ddec87f30dc80df0693a08
jamil86 <veloxbrcaldas@gmail.com>
2020-05-29 15:59:54 -0300

    login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
 require_once("vendor/autoload.php");
+define("BASE_URL","http://localhost/ecommerce/");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,6 +20,21 @@ $app->get('/', function() {
 $app->get('/admin', function() {
     $page = new PageAdmin();
     $page->setTpl("index");
+});
+
+$app->get('/admin/login', function(){
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("login");
+});
+
+$app->post('/admin/login', function(){
+    User::login($_POST["login"], $_POST["password"]);
+    header("Location:" .BASE_URL."admin");
+    exit;
 });
 
 $app->run();
